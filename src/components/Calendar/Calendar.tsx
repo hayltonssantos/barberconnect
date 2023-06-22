6365828cc77c14a308bdbde9fa5439f75e24e3f5
Haylton Santos <aylton.souza10@gmail.com>
2023-06-22 20:35:02 +0100

    I created the logic of appointments by employees

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -1,69 +1,92 @@
 import React, {useState, useContext } from 'react'
 import { generateDate, months } from '../../util/calendar.tsx'
 import cn from '../../util/cn.tsx';
-import dayjs from 'dayjs';
 import {GrFormNext, GrFormPrevious} from 'react-icons/gr'
 import styles from './Calendar.module.scss'
 import { DateContext } from '../../context/date.tsx';
 import { Table } from 'react-bootstrap';
+import { StoreContext } from '../../context/store.tsx';
 
 export default function Calendar({onOffSchedule = false, onOffCalendar = true}) {
 
   const days = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
 
   const {today, setToday, selectDate, setSelectDate, currentDate} : any = useContext(DateContext)
-/*   const currentDate = dayjs()
-  const [today, setToday] = useState(currentDate)
+  const {activeEmployee} : any = useContext(StoreContext) 
 
-  const [selectDate, setSelectDate] = useState(currentDate)
- */
   const marcacoes = [
-    {'name': 'Matheus Silva',
+    { 'func': 'User 1',
+      'name': 'Matheus',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/20'    
+      'date' : '2023/06/22'    
     },
-    {'name': 'Pedro Silva',
+    { 'func': 'User 1',
+      'name': 'Pedro',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/23'   
     },
-    {'name': 'Paulo Silva',
+    { 'func': 'User 2',
+      'name': 'Paulo',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/20'    
+      'date' : '2023/06/22'    
     },
-    {'name': 'Carlos Silva',
+    { 'func': 'User 2',
+      'name': 'Carlos',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/23'   
     },
-    {'name': 'Tiago Silva',
+    { 'func': 'User 1',
+    'name': 'Matheus',
+    'tel': '987657848',
+    'time': '09:30 - 10:00',
+    'date' : '2023/06/22'    
+    },
+    { 'func': 'User 1',
+      'name': 'Pedro',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/20'    
+      'date' : '2023/06/23'   
     },
-    {'name': 'Fernando Silva',
+    { 'func': 'User 2',
+      'name': 'Paulo',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/22'    
     },
-    {'name': 'Fernando Silva',
+    { 'func': 'User 2',
+      'name': 'Carlos',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/23'   
     },
-    {'name': 'Fernando Silva',
+    { 'func': 'User 1',
+    'name': 'Matheus',
+    'tel': '987657848',
+    'time': '09:30 - 10:00',
+    'date' : '2023/06/22'    
+    },
+    { 'func': 'User 1',
+      'name': 'Pedro',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/23'   
     },
-    {'name': 'Fernando Silva',
+    { 'func': 'User 2',
+      'name': 'Paulo',
       'tel': '987657848',
       'time': '09:30 - 10:00',
-      'date' : '2023/06/21'   
+      'date' : '2023/06/22'    
     },
-
+    { 'func': 'User 2',
+      'name': 'Carlos',
+      'tel': '987657848',
+      'time': '09:30 - 10:00',
+      'date' : '2023/06/23'   
+    },
   ]
 
   const noMeeting = () => { 
@@ -72,14 +95,12 @@ export default function Calendar({onOffSchedule = false, onOffCalendar = true}) 
     if (exist === true) {
       return ('')
      }else{
-      return (
-        <tbody>
-          <tr>
-              <td >
-                No more meetings for today
-              </td>
-            </tr>
-        </tbody>
+      return (        
+        <tr id='marcacao' className={styles.marcacaodiv}>
+          <td colSpan={12}>
+            No more meetings for today
+          </td>
+        </tr>
       )
     }
   }
@@ -87,50 +108,57 @@ export default function Calendar({onOffSchedule = false, onOffCalendar = true}) 
   const schedule = (onOffSchedule : any) => {
     if (onOffSchedule === true){
     return (
+      <>
       <div className={styles.schedule}>
-        <h1
-           className={styles.titleSchedule}> 
-           Schedule for {selectDate.toDate().toDateString()}
-        </h1>
-        <Table >
-          <thead>
-              <tr>
-                <th>Funcionário</th>
-                <th>Cliente</th>
-                <th>Telfone</th>
-                <th>Horario</th>
-              </tr>
-            </thead>
-            <tbody>
-              {marcacoes.map((marcado)=>{
-                const dateMarcado = new Date(marcado.date)
-                const aux = []
-                if(dateMarcado.toDateString() === selectDate.toDate().toDateString()){
-                  aux.push(
-                    <tr id='marcacao' className={styles.marcacaodiv}>
-                      <td>{marcado.name}</td>
-                      <td>{marcado.name}</td>
-                      <td>{marcado.tel}</td>
-                      <td>{marcado.time}</td>
-                      {/* <div className={styles.card}>
-                        
-                        <p>Nome: {marcado.name}</p>
-                        <p>Tel: {marcado.tel}</p>
-                        <p>{marcado.time}</p>
-                        
-                      </div> */}
-                      {/* <p className={styles.pName}>Nome: {marcado.name}</p>
-                      <p>Tel: {marcado.tel}</p>
-                    <p>{marcado.time}</p> */}
-                    </tr>
-                  )
-                }
-                return aux
-              })}
-            </tbody>
-        {noMeeting()} 
-        </Table>
-      </div>
+        <div className={styles.titleSchedule}>
+          <h1
+            > 
+            Schedule for {selectDate.toDate().toDateString()}
+          </h1>
+        </div>
+          <Table striped hover>
+            <thead>
+                <tr>
+                  <th>Funcionário</th>
+                  <th>Cliente</th>
+                  <th>Telefone</th>
+                  <th>Horario</th>
+                </tr>
+              </thead>
+              <tbody>
+                {marcacoes.map((marcado)=>{
+                  const dateMarcado = new Date(marcado.date)
+                  const aux = []
+                  if (activeEmployee === 'All'){
+                    if(dateMarcado.toDateString() === selectDate.toDate().toDateString()){
+                      aux.push(
+                        <tr id='marcacao' className={styles.marcacaodiv}>
+                          <td>{marcado.func}</td>
+                          <td>{marcado.name}</td>
+                          <td>{marcado.tel}</td>
+                          <td>{marcado.time}</td>
+                        </tr>
+                      )
+                    }
+                  }else{
+                    if(dateMarcado.toDateString() === selectDate.toDate().toDateString() && (marcado.func === activeEmployee )){
+                      aux.push(
+                        <tr id='marcacao' className={styles.marcacaodiv}>
+                          <td>{marcado.func}</td>
+                          <td>{marcado.name}</td>
+                          <td>{marcado.tel}</td>
+                          <td>{marcado.time}</td>
+                        </tr>
+                      )
+                    }
+                  }  
+                  return aux
+                })}
+                {noMeeting()}   
+              </tbody>
+          </Table>
+        </div>
+      </>
     )}
   }
 
@@ -138,34 +166,36 @@ export default function Calendar({onOffSchedule = false, onOffCalendar = true}) 
     if (onOffCalendar === true){
       return(
         <div className={styles.calendar}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>
-            {months[today.month()]}, {today.year()}
-          </h1>
-          <div className={styles.nextPrevious}>
-            <GrFormPrevious className={styles.next}
+          <div className={styles.header}>
+            <h1 className={styles.title}>
+              {months[today.month()]}, {today.year()}
+            </h1>
+            <div className={styles.nextPrevious}>
+              <GrFormPrevious
+                className={styles.next}
+                onClick={() =>{
+                  setToday(today.month(today.month() - 1))
+                }}
+              />
+              <h1 className={styles.todayHeader} onClick={() =>{
+                  setSelectDate(currentDate)
+                  setToday(currentDate)
+                }}
+              >Today</h1>
+              <GrFormNext className={styles.previous}
               onClick={() =>{
-                setToday(today.month(today.month() - 1))
-              }}
-            />
-            <h1 onClick={() =>{
-                setSelectDate(currentDate)
-              }}
-            >Today</h1>
-            <GrFormNext className={styles.previous}
-            onClick={() =>{
-              setToday(today.month(today.month() + 1))
-            }}/>
+                setToday(today.month(today.month() + 1))
+              }}/>
+            </div>
           </div>
-        </div>
-        <div className={styles.days}>
-          {days.map((day, index)=>{
-            return <h1 key={index} 
-            className={styles.day}>
-              {day}
-              </h1>
-          })}
-        </div>
+          <div className={styles.days}>
+            {days.map((day, index)=>{
+              return <h1 key={index} 
+              className={styles.day}>
+                {day}
+                </h1>
+            })}
+          </div>
 
         <div className={styles.boxDate}>
           
@@ -176,13 +206,14 @@ export default function Calendar({onOffSchedule = false, onOffCalendar = true}) 
                 <h1 className={cn(
                   currentMonth ? "" : `${styles.current}`,
                   
-                  today?~`${styles.today}`:"",
+                  today?`${styles.today}`:"",
 
                   String(selectDate
                     .toDate()
                     .toDateString) === 
-                    date.toDate()
-                      .toDateString() 
+                    
+                    String(date.toDate()
+                      .toDateString()) 
                       ? `${styles.selectDate}`
                       : "",
 
@@ -206,8 +237,12 @@ export default function Calendar({onOffSchedule = false, onOffCalendar = true}) 
   
   return (
     <div className={styles.background}>
-      {calendar(onOffCalendar)}
-      {schedule(onOffSchedule)}
+        {calendar(onOffCalendar)}
+
+      <div className={styles.backSchedule}>
+        {schedule(onOffSchedule)}
+      </div>
+      
     </div>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,31 +5,11 @@ import { Button, Dropdown } from 'react-bootstrap'
 import DropdownMenu from 'react-bootstrap/esm/DropdownMenu'
 import DropdownItem from 'react-bootstrap/esm/DropdownItem'
 import DropdownToggle from 'react-bootstrap/esm/DropdownToggle'
+import { StoreContext } from '../../context/store'
 
 export default function Header(){
     const {appName} : any = useContext(ConfigContext)
-
-    const [Users] = useState([{
-            'id' : 1,
-            'name': 'User 1'
-        },
-        {
-            'id': 2,
-            'name': 'User 2'
-        },
-        {   
-            'id': 3,
-            'name': 'User 3'
-        }    
-    ])
-
-    const firstUser = () =>{
-        const user = Users.map((user) => {if(user.id === 1){return user.name} else {return ''}})
-        return user
-    }
-    const [selectUser, setSelectUser] = useState(firstUser())
-    const activeUser = useMemo(() => (selectUser),[selectUser])
-    
+    const {Employees, setSelectEmployee, activeEmployee} : any = useContext(StoreContext)
     return(
         <header className={`${styles.header} align-items-center`}>
             <div className={styles.divTitle}>
@@ -38,12 +18,12 @@ export default function Header(){
            
             <Dropdown className={styles.user}>
                 <DropdownToggle variant='success' id='dropdown-basic' className={styles.dropdown}>
-                    {activeUser}
+                    {activeEmployee}
                 </DropdownToggle>
                     <DropdownMenu>
-                        {Users.map((user:any) => (
+                        {Employees.map((user:any) => (
                             <DropdownItem>
-                                <Button className={styles.dropdownItens} onClick={() => setSelectUser(user.name)}>
+                                <Button className={styles.dropdownItens} onClick={() => setSelectEmployee(user.name)}>
                                     {user.name}
                                 </Button>    
                             </DropdownItem>
